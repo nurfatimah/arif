14716f7975b5842922af48cc54dd35ec86b33993
nurfatimah <48873225+nurfatimah@users.noreply.github.com>
2019-03-24 23:01:52 +0700

    Add files via upload

---
+++ helloword.php
@@ -0,0 +1,20 @@
+<?php
+    echo"<h1>Hello World</h1> <br/>";
+    
+    $text = "<table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0064</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>Dimas Ahmad Rizal</td>
+    </tr>
+</table> <br>";
+
+    echo $text;
+?>
+
+    <a href="/akademik">Go To Akademik</a>
